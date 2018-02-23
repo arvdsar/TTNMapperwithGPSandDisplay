b2eb18ac14d9f0411b037e0f369dbf51b41c23f9
Alexander <alexander@vdsar.net>
2018-02-23 18:29:54 +0100

    updated comments

--- TTN_GPS_Mapper_v03_Public.ino
+++ TTN_GPS_Mapper_v03_Public.ino
@@ -4,6 +4,8 @@
  * Author: JP Meijers
  * Date: 2016-09-07
  *
+ * Modified by Alexander van der Sar
+ * Date: 22-2-2018
  * Modified for using the program with Arduino Mega (4x hardware serialport,
  * RN2483 LoRa module, 0.96" SPI Oled display and UBlox 6 serial GPS.
  * You can find a 3D printable enclosure to fit this all at:
